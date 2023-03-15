b09245c5d97b29e66f9b20dfe2407d2b014f6b3d
[Geletei Daniel] <[therulezz34@gmail.com]>
2023-03-15 19:29:08 +0100

    házi feladat

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hf-javascript-01
+
 JavaScript házi feladat
+
+Ebben a repoban az első házi feladatomat mutatom be.
+
+Az alkalmazás bekéri hogy hány jegyet akarsz vásárolni, ez után azt hogy milyen típusú jegyet (Diák/Felnőtt)
+Ha tíznél több jegyet vásárolsz, akkor 10% kedvezményt kapsz!
---
+++ main.js
@@ -0,0 +1,33 @@
+// Jegy árak meghatározása
+let felnottJegy = 1900;
+let diakJegy = 1100;
+
+// Jegy mennyiségének bekérése
+let jegyMennyiseg = prompt("Mennyi jegyet szeretnél vásárolni?");
+
+// Jegy mennyiségének vizsgálata. Jogosult-e a 10% kedvezményre?
+if (jegyMennyiseg > 10) {
+  (felnottJegy = felnottJegy * 0.9) && (diakJegy = diakJegy * 0.9);
+} else {
+  (felnottJegy = felnottJegy * 1) && (diakJegy = diakJegy * 1);
+}
+
+// Jegy típusának megadása
+let jegyTipus = prompt(
+  "Milyen típusú jegyet szeretnél vásárolni? Diák/Felnőtt"
+);
+
+// eredmeny változó meghatározása
+let eredmeny = 0;
+
+// Jegyek mennyiségének és típusának megvizsgálása, kiszámítása
+if (jegyTipus === "Diák") {
+  eredmeny = diakJegy * jegyMennyiseg;
+} else if (jegyTipus === "Felnőtt") {
+  eredmeny = felnottJegy * jegyMennyiseg;
+} else {
+  alert("Nem jó az adat formátuma! Kérlek próbáld újra!");
+}
+
+// Eredmény kiíratása konzolba
+console.log(eredmeny + " Ft-ba fog kerülni.");
